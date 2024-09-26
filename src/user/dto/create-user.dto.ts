cdb9ae4cc4dbcdcf16d5a4631ab2db39de5baf9b
jefaokpta <jefaokpta@hotmail.com>
2024-09-26 16:26:48 -0300

    validacao ok

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,17 @@
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+   @IsOptional()
    id: number;
+
+   @IsOptional()
    name: string;
+
+   @IsNotEmpty({ message: 'email obrigatorio' })
+   @IsEmail({}, { message: 'email invalido' })
    email: string;
+
+   @IsNotEmpty()
+   @MinLength(6, { message: 'senha deve ter no minimo 6 caracteres' })
    password: string;
 }
