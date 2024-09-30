b8011bff351bd164f94764ee06f487c5f9328076
jefaokpta <jefaokpta@hotmail.com>
2024-09-30 13:58:39 -0300

    carregando dados do banco e mostrando via VueJS

--- src/pages/controllers/vuejs.controller.ts
+++ src/pages/controllers/vuejs.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get, Render } from '@nestjs/common';
+import { UserService } from '../../user/user.service';
 
 @Controller('vuejs')
 export class VuejsController {
+  constructor(private readonly userService: UserService) {}
 
   @Get()
   @Render('vuejs')
-  load() {
-    return { name: 'Pagina VueJS com PrimeVue' };
+  async load() {
+    return {
+      name: 'Pagina VueJS com PrimeVue',
+    };
   }
 
+  @Get('users')
+  async users() {
+    return await this.userService.findAll();
+  }
 }
