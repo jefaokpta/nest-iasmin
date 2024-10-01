2179a0368862aba75ca92ca69c3b13d69268ab1c
jefaokpta <jefaokpta@hotmail.com>
2024-10-01 11:00:41 -0300

    de volta aos trilhos

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
@@ -16,8 +15,8 @@ async function bootstrap() {
     transform: true,
   }));
 
-  app.useStaticAssets(join(process.cwd(), './public'));
-  app.setBaseViewsDir(join(process.cwd(), './views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);
